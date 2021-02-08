27c4be524fb99a0fe5831c8e950b56a3d160ff29
Chris Bird (kvogel@kvogel-elitebook) <chrisjbird@gmail.com>
2021-02-08 20:53:50 +0000

    get_platform(); show mtime of src file

--- src/dev.py
+++ src/dev.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import SessionNotCreatedException, StaleElementReferenceException
 # from selenium.common import SessionNotCreatedException, StaleElementReferenceException
 import time
+import os, platform, sys, time
 
 # def get_page():
 #     chrome = webdriver.Chrome()
@@ -79,14 +81,48 @@ class TabSutra():
 
         self.browser.close()
 
-
-# selenium.common.exceptions.StaleElementReferenceException: Message: stale element reference: element is not attached to the page document
-#   (Session info: chrome=83.0.4103.61)
-
-# TODO ptprompt?
+        # selenium.common.exceptions.StaleElementReferenceException: Message: stale element reference: element is not attached to the page document
+        #   (Session info: chrome=83.0.4103.61)
 
     def go(self):
         try:
+            # C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
+            # chrome.exe current path:
+            # '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+            # chromedriver current path:
+            # /home/kvogel/.local/bin/chromedriver
+
+            this_file_path = os.path.abspath(__file__)
+                # os.path.dirname(os.path.abspath(__file__))
+            this_file_mtime = os.path.getmtime(this_file_path)
+
+            # print(f'this_file_mtime: {this_file_mtime}') #  1612816979.1176763
+            # time_now = time.time()
+            # print(f'time_now {time_now}') # 1612816983.5057838
+            # time_now = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+            # print(f'time_now {time_now}') # 2021-02-08
+            # print('boo asfasdfasdfasd xxx!')
+
+            this_file_mtime_hr = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(this_file_mtime))
+            print(f'this is {this_file_path}, modified at {this_file_mtime_hr}')
+                # this is /home/kvogel/projects/tabsutra/src/dev.py, modified at 2021-02-08 20:44:07
+
+            print(f'go(): Platform: {self.get_platform()}')
+            print()
+            sys.exit(0)
+
+            options = Options()
+
+            # if self.get_platform() == 
+            options.binary_location = "/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+            driver = webdriver.Chrome(options=options, executable_path="/home/kvogel/.local/bin/chromedriver", )
+            # otherwise on WSL get: `selenium.common.exceptions.WebDriverException: Message: unknown error: cannot find Chrome binary`
+            # even if `chrome.exe` is in PATH
+
+            # options.binary_location = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+            # driver = webdriver.Chrome(chrome_options=options, executable_path="C:/Utility/BrowserDrivers/chromedriver.exe", )
+            # driver.get('http://google.com/')
+
             # page = get_page()
             # self.browser = self.get_browser()
             # self.parse_page()
@@ -98,8 +134,31 @@ class TabSutra():
             print(e.message)
             return
 
+    def get_platform(self):
+        # print(f'platform.system(): {platform.system()}')
+        if platform.system() == 'Linux':
+        # if sys.platform == 'linux':
+            # Linux or WSL
+            # uname = os.uname()
+            # if uname.sysname == 'Linux':
+            if 'microsoft-standard' in os.uname().release:
+                return 'wsl'
+            else:
+                return 'linux'
+        # elif uname.sysname == 'Darwin':
+        # elif sys.platform == 'darwin':
+        elif platform.system() == 'Darwin':
+            return 'macos'
+        # elif uname.sysname
+        elif platform.system() == 'Windows':
+            return 'windows'
+            # print(f'uname: {uname.')
+
+
 def main():
     tabsutra = TabSutra()
+    print(f'main(): Platform: {tabsutra.get_platform()}')
+    # sys.exit(0)
     tabsutra.go()
 
 if __name__ == "__main__":
