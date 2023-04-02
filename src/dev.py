509f2ef43ed8078f96b7d116616effd180192ab4
Chris Bird <chrisjbird@gmail.com>
2023-04-02 16:17:06 +0100

    dev: save backups to timestamped file in backups folder

--- src/dev.py
+++ src/dev.py
@@ -4,10 +4,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import SessionNotCreatedException, StaleElementReferenceException
-import time
+import datetime
 import os, platform, sys, time
+import shutil
+import usb.core
+import usb.util
 
-# TODO check if adb installed
+# TODO check if adb installed? Is adb necessary? maybe if you want to check if a phone is attached
 # TODO run adb devices, check if device
 # TODO make sure phone is attached
 # TODO make sure phone data is allowed
@@ -22,10 +25,6 @@ class TabSutra():
     CHROME_INSPECT_URL = 'chrome://inspect/#devices'
     SLEEP_SECONDS = 3
 
-    # jls_extract_var = 'chrome://inspect/#devices'
-    # CHROME_INSPECT_URL = jls_extract_var
-    # ??? Java linting my Python file?? : vscode (https://www.reddit.com/r/vscode/comments/n05vl9/java_linting_my_python_file/)
-
     def __init__(self):
         pass
 
@@ -33,6 +32,14 @@ class TabSutra():
         self.driver = webdriver.Chrome()
 
     def parse_page_with_sleep(self):
+
+        out_file_name = "backups/tabs-" + datetime.datetime.now().replace(microsecond=0).isoformat().replace(':', '') + ".md"
+        print(f"filename: {out_file_name}")
+
+        dev = usb.core.find(idVendor=0xfffe, idProduct=0x0001)
+
+        exit()
+
         options = Options()
         # options.add_argument("--headless") # len(subrows): 0 No rows fetched. Quitting.
         options.add_argument("--no-sandbox")
@@ -44,6 +51,7 @@ class TabSutra():
         self.driver.get(self.CHROME_INSPECT_URL)
         print(f'sleeping for {self.SLEEP_SECONDS} seconds...')
         print(f'TODO should poll to see if browser ready, not sleep. Or at least prompt.')
+        # TODO should poll to see if browser ready, not sleep. Or at least prompt.
         # time.sleep(self.SLEEP_SECONDS) #
         input("Make sure access permissions are granted on, then press a key to continue")
         # Popup on phone: "Allow USB debugging?"
@@ -63,9 +71,10 @@ class TabSutra():
 
         # for elem in self.browser.find_elements_by_class_name('subrow'):
         try:
-            with open('tabs.md', 'w') as out_file:
+            with open(out_file_name, 'w') as out_file:
                 for elem in subrows:
-                    print(elem.text)
+                    # print(elem.text)
+                    print(".", end="")
                     lines = elem.text.split('\n')
                     if len(lines) > 1:
                         title = lines[0]
@@ -113,7 +122,7 @@ class TabSutra():
             except StaleElementReferenceException as e:
                 print(f'e: {e.message}')
                 print('Not yet...')
-            
+
 
         print('outside loop')
         # for row in rows:
@@ -138,9 +147,15 @@ class TabSutra():
             this_file_path = os.path.abspath(__file__) # os.path.dirname(os.path.abspath(__file__))
             this_file_mtime = os.path.getmtime(this_file_path)
             this_file_mtime_hr = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(this_file_mtime))
-            print(f'This is {this_file_path}, modified at {this_file_mtime_hr}')
-            print(f'Platform: {self.get_platform()}')
+            # print(f'This is {this_file_path}, modified at {this_file_mtime_hr}')
+            # print(f'Platform: {self.get_platform()}')
             print()
+
+            adb_path = shutil.which("adb")
+            if adb_path == None:
+                print("adb not found (does this matter?)")
+            else:
+                print(f"adb found at {adb_path}")
 
             options = Options()
             options.add_argument("--headless")
@@ -179,9 +194,9 @@ class TabSutra():
 
 def main():
     print(f'This is {os.path.abspath(__file__)} on Python {sys.version}')
-    res = input("Proceed? y/n: ")
-    if res != 'y':
-        exit(0)
+    # res = input("Proceed? y/n: ")
+    # if res != 'y':
+    #     exit(0)
     # print('OK, but quitting anyway')
     # exit(0)
     tabsutra = TabSutra()
